8c72f3335a2a34e82d993c83f5ddd06d0e1fef46
eucarizan <eucarizan@gmail.com>
2025-02-13 19:20:47 +0800

    connecting to a json file done

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
+const URL = 'http://127.0.0.1:8080/question.json';
+
+let questions = [];
 
 const game = async () => {
-  // write your code here
+  let container = document.getElementById("container");
+  fetch(URL)
+    .then((response) => response.json())
+    .then((json) => {
+      questions.push(JSON.stringify(json));
+      container.innerHTML = questions;
+    })
+    .catch((error) => console.log(error));
 }
 
 game();
